28db322845165a46eca547dcd5d31e3de38c2a02
BilgeTuna <92364036+BilgeTuna@users.noreply.github.com>
2023-01-26 14:35:50 +0300

    update

---
+++ TheGameCircle-Task1/Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+    private int superJumpsRemaining;
+    private float speed = 2f;
+
+    private void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+        horizontalInput = Input.GetAxis("Horizontal") * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
+
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+
+        if (jumpKeyWasPressed)
+        {
+            float jumpPower = 5f;
+            if (superJumpsRemaining > 0)
+            {
+                jumpPower *= 2;
+                superJumpsRemaining--;
+            }
+            rigidbodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7)
+        {
+            Destroy(other.gameObject);
+            superJumpsRemaining++;
+        }
+
+        if (other.gameObject.layer == 8)
+        {
+            Destroy(other.gameObject);
+            speed++;
+        }
+
+        if (other.gameObject.layer == 9)
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+
+    }
+
+}
